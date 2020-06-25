c59380f7e3f5bfccd87ccab315b1f7e416ec75f6
curviture <gevghazaryan@aol.com>
2020-06-25 21:40:45 +0400

    Learning about css and file loaders

--- 1.simple/src/index.js
+++ 1.simple/src/index.js
@@ -1,1 +1,10 @@
+import './style.css';
+
+const div = document.createElement('div');
+div.classList.add('red');
+
+div.textContent = 'Hello kitty';
+
+document.body.appendChild(div);
+
 console.log('hello kitty');

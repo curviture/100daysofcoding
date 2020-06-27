7af6897cccd7eea3089e8dfaa201fe6e10b84fa0
curviture <gevghazaryan@aol.com>
2020-06-28 02:30:54 +0400

    styling navigation bar

--- 1.simple/src/index.js
+++ 1.simple/src/index.js
@@ -1,4 +1,6 @@
 import './style.css';
+import 'typed.js';
+import Typed from 'typed.js';
 
 const div = document.createElement('div');
 div.classList.add('red');
@@ -7,4 +9,24 @@ div.textContent = 'Hello kitty';
 
 document.body.appendChild(div);
 
-console.log('hello kitty');
+//this part of script is about using typed js
+
+const typedOpt = {
+    loop: true,
+    loopCount: Infinity
+}
+
+const typedStrings = [
+    'Gevorg',
+    "Georgiy",
+    'Jorgen',
+    'Gaggo'
+]
+
+typedOpt.strings = typedStrings;
+
+const typedElm = document.getElementById('names');
+
+let typed = new Typed(typedElm, typedOpt);
+
+

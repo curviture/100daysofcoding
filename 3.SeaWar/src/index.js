afde244d871c41f787b8a3ca0d34bbcb7081c38c
curviture <gevghazaryan@aol.com>
2020-07-26 17:41:07 +0400

    Continuing to develop Seawar game, added some styling, also board generation for both players

--- 3.SeaWar/src/index.js
+++ 3.SeaWar/src/index.js
@@ -1,11 +1,48 @@
+import './style.css';
+
 class SeaWars {
     constructor(player1, player2) {
         this.p1 = new Player('p1', player1);
         this.p2 = new Player('p2', player2);
+        this.createBoard();
         this.playersTurn = this.p1;
     }
-    swapPlayer() {
+    nextPlayer() {
         this.playersTurn = this.playersTurn == this.p1 ? this.p2 : this.p1;
+    }
+    createBoard(size = {height: 10, width: 10}) {
+        let {height, width} = size;
+        this.p1.board = new Board({height, width})
+        this.p2.board = new Board({height, width})
+    }
+    renderBoard(player) {
+        let d = new DomMan();
+        let boardCont = d.createElm('div');
+        boardCont.id = player.player;
+        d.appendToParent(boardCont, document.querySelector('.game-container'));
+        boardCont.classList.add('board-container');
+        for(let row of player.board) {
+            for(let cell of row) {
+                let gridCell = d.createElm('div');
+                gridCell.classList.add('grid-cell')
+                if(cell == "") {
+                    gridCell.classList.add('empty')
+                } else if(cell == "o") {
+                    gridCell.classList.add('ship')
+                } else {
+                    gridCell.classList.add('debris')
+                }
+                d.appendToParent(gridCell, boardCont);
+            }
+        }
+    }
+    render() {
+        let d = new DomMan();
+        let gC = d.createElm('div');
+        gC.classList.add('game-container');
+        d.appendToParent(gC, document.body);
+        this.renderBoard(this.p1)
+        this.renderBoard(this.p2)        
     }
 }
 
@@ -14,9 +51,15 @@ class Player {
         let d = new DomMan();
         this.player = player;
         this.name = name;
-        this.board = d.createElm('div');
-        this.board.id = player;
-        d.appendToParent(this.board, document.body);
+
+    }
+}
+
+class Board {
+    constructor(props) {
+        this.height = props.height;
+        this.width = props.width;
+        this.grid = new Array(10).fill(new Array(10).fill(''));
     }
 }
 
@@ -35,4 +78,5 @@ class DomMan {
 }
 
 let game = new SeaWars('kitty', 'doggo');
-console.log(game);
+game.createBoard();
+game.render()
